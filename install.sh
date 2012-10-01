f0caae7420986b1f61992fb34ce0bb96d20eb2f7
Kilew <tt.kilew@gmail.com>
2012-10-01 11:04:43 +0300

    Added script for installing templates

---
+++ install.sh
@@ -0,0 +1,11 @@
+echo "This script is installing Color schemes, Live Templates and CodeStyles for Appcode"
+
+APPCODE_PREFS_DIR=~/Library/Preferences/appCode10
+
+echo  "Copying files to ${APPCODE_PREFS_DIR}"
+
+cp -R codestyles ${APPCODE_PREFS_DIR}
+cp -R colors ${APPCODE_PREFS_DIR}
+cp -R templates ${APPCODE_PREFS_DIR}
+
+echo "Done"
