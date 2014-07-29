aee1e6a0df93c030c7aeb67c355da62dd9f4fd4c
Paul Taykalo <tt.kilew@gmail.com>
2014-07-29 12:20:10 +0300

    Updated installation script

--- install-snippet.sh
+++ install-snippet.sh
@@ -13,14 +13,23 @@ if [[ ! -f $1 ]]; then
 	exit
 fi	
 
-APPCODE_PREFS_DIR=~/Library/Preferences/appCode20/templates
-echo  "Copying file '$1' to ${APPCODE_PREFS_DIR}"
+for i in $(seq 2 9); 
+  do 
+    
+    APPCODE_SETTINGS_DIR="${HOME}/Library/Preferences/appCode${i}0"
+    if [ -d "${APPCODE_SETTINGS_DIR}" ]; then
+    	APPCODE_PREFS_DIR="${HOME}/Library/Preferences/appCode${i}0/templates"
+		echo  "Copying file '$1' to ${APPCODE_PREFS_DIR}"
 
-if [[ ! -e $APPCODE_PREFS_DIR ]]; then
-    mkdir $APPCODE_PREFS_DIR
-fi
+		if [[ ! -e $APPCODE_PREFS_DIR ]]; then
+		    mkdir $APPCODE_PREFS_DIR
+		fi
 
-cp "$1" $APPCODE_PREFS_DIR
+		cp "$1" $APPCODE_PREFS_DIR
+	fi
+
+done
+
 
 echo "Restart AppCode to apply changes"
 echo
